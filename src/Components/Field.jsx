decd4854f3dd3a07d501a96e151a5a58aca4759a
GabrielRosendo27 <gabriel_eulalio@hotmail.com>
2024-10-08 09:20:37 -0300

    prop

--- src/Components/Field.jsx
+++ src/Components/Field.jsx
@@ -1,29 +1,30 @@
 import React from "react";
 
-const data = {
-  date: "Data",
-  exercicio: "Selecione o treino",
-};
 const treinos = {
   a: "Pernas",
   b: "Costas e Bíceps",
   c: "Peito e Tríceps",
   d: "Ombro",
 };
+
 const Field = () => {
   const [treino, setTreino] = React.useState(false);
+  const dataAtual = new Date();
+  function handleChange() {
+    console.log("a");
+  }
   return (
     <form>
-      <label>{data.date}</label>
+      <label>{dataAtual.toLocaleDateString("pt-BR")}</label>
       <br />
-      <select>
-        <option value="" disabled>
-          {data.exercicio}
+      <select value={"Selecione o Exercício"} onChange={handleChange}>
+        <option value="Selecione o Exercício" disabled>
+          Selecione o Exercício
         </option>
-        <option value="">{treinos.a}</option>
-        <option value="">{treinos.b}</option>
-        <option value="">{treinos.c}</option>
-        <option value="">{treinos.d}</option>
+        <option value="a">{treinos.a}</option>
+        <option value="b">{treinos.b}</option>
+        <option value="c">{treinos.c}</option>
+        <option value="d">{treinos.d}</option>
       </select>
     </form>
   );
